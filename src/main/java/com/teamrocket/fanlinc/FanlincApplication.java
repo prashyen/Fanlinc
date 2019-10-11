4e87a2422ecf4f831563e4fe2c382fe9e8e531a3
Carl Alvares <alvares.carl@gmail.com>
2019-10-10 20:13:16 -0400

    Initialize Spring application with Spring Web and PostgreSQL Driver

---
+++ src/main/java/com/teamrocket/fanlinc/FanlincApplication.java
@@ -0,0 +1,13 @@
+package com.teamrocket.fanlinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanlincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanlincApplication.class, args);
+    }
+
+}
