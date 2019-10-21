b30c339885f0626bd41d2bb12a6a23330cb28d72
RahulRamani23 <39231376+RahulRamani23@users.noreply.github.com>
2019-10-21 01:09:23 -0400

    Made some fixes to Account services based on PR comments

--- BackEnd/src/main/java/com/teamrocket/fanlinc/services/AccountService.java
+++ BackEnd/src/main/java/com/teamrocket/fanlinc/services/AccountService.java
@@ -14,52 +14,56 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AccountService {
 
-    private UserRepository userRepository;
+  private UserRepository userRepository;
 
-    public AccountService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+  public AccountService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  /**
+   * Checks if user with given username is present in database, and if so, checks if the given
+   * password matches the one stored for that user
+   *
+   * @param request a {@link ValidateUserRequest} object containing a username and a password
+   * @return a {@link ValidateUserResponse} object containing the given username and whether or not
+   * the given password matches the stored password
+   * @throws UserNotFoundException if user with given username was not found
+   */
+  @Transactional(readOnly = true)
+  public ValidateUserResponse validateUser(ValidateUserRequest request) {
+    User requestedUser = userRepository.findByUsername(request.getUsername());
+    // if the repository method returns a null value, user with given username was not found
+    if (requestedUser == null) {
+      throw new UserNotFoundException("User with username " + request.getUsername() + " not found");
     }
+    // compare given password with password stored in the database and save this result in the response object
+    return new ValidateUserResponse(request.getUsername(),
+        requestedUser.getPassword().equals(request.getPassword()));
+  }
 
-    /**
-     * Checks if user with given username is present in database, and if so, checks if the given password matches the
-     * one stored for that user
-     *
-     * @param request a {@link ValidateUserRequest} object containing a username and a password
-     * @return a {@link ValidateUserResponse} object containing the given username and whether or not the given password matches the stored password
-     * @throws UserNotFoundException if user with given username was not found
-     */
-    @Transactional(readOnly = true)
-    public ValidateUserResponse validateUser(ValidateUserRequest request) {
-        User requestedUser = userRepository.findByUsername(request.getUsername());
-        // if the repository method returns a null value, user with given username was not found
-        if (requestedUser == null) {
-            throw new UserNotFoundException("User with username " + request.getUsername() + " not found");
-        }
-        // compare given password with password stored in the database and save this result in the response object
-        return new ValidateUserResponse(request.getUsername(), requestedUser.getPassword().equals(request.getPassword()));
+  /**
+   * Checks if a username with a given username exists in the database and if not it will add the
+   * user
+   *
+   * @param request a {@link AddUserRequest} object containing all user registration information
+   * @return a {@link AddUserResponse} object containing the given username of the new user
+   * @throws UsernameNotUniqueException if user with given username already exists
+   */
+  @Transactional(readOnly = false)
+  public AddUserResponse addUser(AddUserRequest request) {
+    // check to see if the user with the given username already exists
+    User requestedUser = userRepository.findByUsername(request.getUsername());
+    // ensure the requested username is unique if it is not throw an exception
+    if (requestedUser != null) {
+      throw new UsernameNotUniqueException(
+          "User with username " + request.getUsername() + " already exists");
     }
+    // otherwise create a new user and save the user into the repo
+    userRepository.save(new User(request.getUsername(), request.getPassword(),
+        request.getFirstName(), request.getLastName(), request.getDateOfBirth(),
+        request.getLocation(),
+        request.getBio(), request.getProfilePhotoUrl()));
+    return new AddUserResponse(request.getUsername());
 
-    /**
-     * Checks if a username with a given username exists in the database and if not it will add the user
-     *
-     * @param request a {@link ValidateUserRequest} object containing all user registration information
-     * @return a {@link AddUserResponse} object containing the given username of the new user
-     * @throws UsernameNotUniqueException if user with given username already exists
-     */
-    @Transactional(readOnly = true)
-    public AddUserResponse addUser(AddUserRequest request){
-        // check to see if the user with the given username already exists
-        User requestedUser = this.userRepository.findByUsername(request.getUsername());
-        // ensure the requested username is unique if it is not throw an exception
-        if (requestedUser != null){
-            throw new UsernameNotUniqueException(
-                "User with username " + request.getUsername() + "already exists");
-        }
-        // otherwise create a new user and save the user into the repo
-        User newUser =  this.userRepository.save(new User(request.getUsername(), request.getPassword(),
-            request.getFirstName(), request.getLastName(), request.getDateOfBirth(),request.getLocation(),
-            request.getBio(), request.getProfilePhotoUrl()));
-        return new AddUserResponse(request.getUsername());
-
-    }
+  }
 }
