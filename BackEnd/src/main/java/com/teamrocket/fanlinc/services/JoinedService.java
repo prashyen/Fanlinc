42e6006ed8ab279bebfc14a3b7f7781100c7ccc0
prashyen <37853402+prashyen@users.noreply.github.com>
2019-10-30 01:33:16 -0400

    TEAM-32 - added error handling for missing user or fandom

--- BackEnd/src/main/java/com/teamrocket/fanlinc/services/JoinedService.java
+++ BackEnd/src/main/java/com/teamrocket/fanlinc/services/JoinedService.java
@@ -1,7 +1,13 @@
 package com.teamrocket.fanlinc.services;
 
+import com.teamrocket.fanlinc.exceptions.FandomAlreadyExistsException;
 import com.teamrocket.fanlinc.exceptions.UserAlreadyJoinedFandomException;
+import com.teamrocket.fanlinc.exceptions.UserNotFoundException;
+import com.teamrocket.fanlinc.models.Fandom;
+import com.teamrocket.fanlinc.models.User;
+import com.teamrocket.fanlinc.repositories.FandomRepository;
 import com.teamrocket.fanlinc.repositories.JoinedRepository;
+import com.teamrocket.fanlinc.repositories.UserRepository;
 import com.teamrocket.fanlinc.requests.JoinedFandomRequest;
 import com.teamrocket.fanlinc.responses.AddJoinedFandomResponse;
 import org.springframework.stereotype.Service;
@@ -11,9 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class JoinedService {
 
     private JoinedRepository joinedRepository;
+    private UserRepository userRepository;
+    private FandomRepository fandomRepository;
 
-    public JoinedService(JoinedRepository joinedRepository) {
+    public JoinedService(JoinedRepository joinedRepository, UserRepository userRepository, FandomRepository fandomRepository) {
         this.joinedRepository = joinedRepository;
+        this.userRepository = userRepository;
+        this.fandomRepository = fandomRepository;
     }
 
 
@@ -21,7 +31,19 @@ public class JoinedService {
     public AddJoinedFandomResponse addJoinedFandom(JoinedFandomRequest request) {
         String requestedRelationType = joinedRepository.findJoinedByUsernameAndFandomName(request.getUsername(),
                                                                                             request.getFandomName());
+        User requestedUser = userRepository.findByUsername(request.getUsername());
+        // if the repository method returns a null value, user with given username was not found
+        if (requestedUser == null) {
+            throw new UserNotFoundException("User with username " + request.getUsername() + " not found");
+        }
+        Fandom requestedFandom = fandomRepository.findByFandomName(request.getFandomName());
 
+        //ensure the requested fandom hasn't already been created
+        if (requestedFandom != null) {
+            // if the requested fandom isn't unique output exception
+            throw new FandomAlreadyExistsException(
+                    "A fandom with the name " + request.getFandomName() + " already exists");
+        }
         //ensure the user is not already a member of the fandom
         if (requestedRelationType != null) {
             // if the user already a member of the fandom output exception
