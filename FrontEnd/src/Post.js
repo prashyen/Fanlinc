a3400c0a4834b3977d1713acad4f5744fef8b6d7
Ruidan Ji <ji.ruidan@gmail.com>
2019-11-01 19:52:37 -0400

    TEAM-38 - created dialog for each post

--- FrontEnd/src/Post.js
+++ FrontEnd/src/Post.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -14,41 +12,28 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Hidden from '@material-ui/core/Hidden';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 import Container from '@material-ui/core/Container';
-//import Markdown from './Markdown';
-//import post1 from './blog-post.1.md';
-//import post2 from './blog-post.2.md';
-//import post3 from './blog-post.3.md';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © Fanlinc 2019. '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
+      {'Copyright © Fanlinc'}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
 }
 
+
+
 const useStyles = makeStyles(theme => ({
-  toolbar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbarTitle: {
-    flex: 1,
-  },
-  toolbarSecondary: {
-    justifyContent: 'space-between',
-    overflowX: 'auto',
-  },
-  toolbarLink: {
-    padding: theme.spacing(1),
-    flexShrink: 0,
-  },
   mainFeaturedPost: {
     position: 'relative',
     backgroundColor: theme.palette.grey[800],
@@ -58,43 +43,17 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
   },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    backgroundColor: 'rgba(100,100,0,.3)',
-  },
-  mainFeaturedPostContent: {
-    position: 'relative',
-    [theme.breakpoints.up('md')]: {
-      paddingRight: 0,
-    },
-  },
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
   card: {
-    display: 'flex',
-    alignItems: "center"
+     display: 'block',
+     width: '55vw',
+     transitionDuration: '0.3s',
+     height: '15vw'
   },
   cardDetails: {
     flex: 1,
   },
   cardMedia: {
     width: 160,
-  },
-//  markdown: {
-//    ...theme.typography.body2,
-//    padding: theme.spacing(3, 0),
-//  },
-  sidebarAboutBox: {
-    padding: theme.spacing(2),
-    backgroundColor: theme.palette.grey[200],
-  },
-  sidebarSection: {
-    marginTop: theme.spacing(3),
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
@@ -103,100 +62,88 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const sections = [
-  'Home',
-  'Account',
-  'Explore'
-];
-
 const featuredPosts = [
   {
-    title: 'Featured post',
+    title: 'Featured post 6',
     date: 'Nov 12',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      'This will be Fanlinc post 6 example of longer post with supporting text, images, links or other sources below:'
+      + 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from'
+      +'45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up'
+      +'one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,'
+      +'discovered the undoubtable source'
   },
   {
-    title: 'Post title',
+    title: 'Post title 5',
     date: 'Nov 11',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      'This will be Fanlinc post 5 with supporting text, shorter post'
   },
+{
+  title: 'Featured post 4',
+  date: 'Nov 3',
+  description:
+    'This will be Fanlinc post 4 with supporting text, images, links or other sources below',
+},
+{
+  title: 'Post title 3',
+  date: 'Nov 2',
+  description:
+    'This will be Fanlinc post 3 with supporting text, images, links or other sources below',
+  },
+ {
+   title: 'Featured post 2',
+   date: 'Oct 19',
+   description:
+     'This will be Fanlinc post 2 with supporting text, images, links or other sources below',
+ },
+ {
+   title: 'Post title 1',
+   date: 'Oct 17',
+   description:
+     'This will be Fanlinc post 1 with supporting text, images, links or other sources below',
+ }
 ];
 
-//const posts = [post1, post2, post3];
 
-const archives = [
-  'March 2020',
-  'February 2020',
-  'January 2020',
-  'December 2019',
-  'November 2019',
-  'October 2019',
-  'September 2019',
-  'August 2019',
-  'July 2019',
-  'June 2019',
-  'May 2019',
-  'April 2019',
-];
 
-const social = ['GitHub', 'Twitter', 'Facebook'];
 
 export default function Blog() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false)
+  const [title, setTitle] = React.useState('')
+  const [content, setContent] = React.useState('')
+  const [date, setDate] = React.useState('')
 
+  const clickHandleOpen = (title, content, date) => {
+    setOpen(true)
+    setTitle(title)
+    setContent(content)
+    setDate(date)
+  };
+
+  const clickHandleClose = () => {
+    setOpen(false);
+  }
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Toolbar className={classes.toolbar}>
-          <Typography
-            component="h2"
-            variant="h5"
-            color="inherit"
-            align="center"
-            noWrap
-            className={classes.toolbarTitle}
-          >
-            HomeFeed
-          </Typography>
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
-          <Button variant="outlined" size="small">
-            My Account
-          </Button>
-        </Toolbar>
-        <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-          {sections.map(section => (
-            <Link
-              color="inherit"
-              noWrap
-              key={section}
-              variant="body2"
-              href="#"
-              className={classes.toolbarLink}
-            >
-              {section}
-            </Link>
-          ))}
-        </Toolbar>
 
           <Grid container direction="column" alignItems="center" spacing= {2} style={{ minHeight: '80vh' }}>
             {featuredPosts.map(post => (
-              <Grid item key={post.title} xs={12} alignItems="center" direction="column" justify="flex-start">
-                <CardActionArea component="a" href="#">
-                  <Card className={classes.card} alignItems="center">
-                    <div className={classes.cardDetails} alignItems="center">
-                      <CardContent alignItems="center">
+              <Grid item key={post.title} xs={12} >
+                <CardActionArea onClick = {() => clickHandleOpen(post.title, post.description, post.date)}>
+                  <Card className={classes.card} >
+                    <div className={classes.cardDetails} >
+                      <CardContent >
                         <Typography component="h2" variant="h5">
                           {post.title}
                         </Typography>
                         <Typography variant="subtitle1" color="textSecondary">
                           {post.date}
                         </Typography>
-                        <Typography variant="subtitle1" paragraph>
+                        <Typography variant="subtitle1" noWrap={true} paragraph>
                           {post.description}
                         </Typography>
                         <Typography variant="subtitle1" color="primary">
@@ -204,27 +151,37 @@ export default function Blog() {
                         </Typography>
                       </CardContent>
                     </div>
-                    <Hidden xsDown>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image=""
-                        title="Image title"
-                      />
-                    </Hidden>
                   </Card>
                 </CardActionArea>
+                <Dialog
+                  open={open}
+                  onClose={clickHandleClose}
+                >
+
+                <DialogTitle >{title}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>{content}</DialogContentText>
+                </DialogContent>
+                   <DialogActions>
+                     <Button onClick={clickHandleClose} color="primary">
+                        Close
+                     </Button>
+                    </DialogActions>
+                </Dialog>
               </Grid>
+
+
             ))}
           </Grid>
           {/* End sub featured posts */}
-          <Grid container spacing={5} xs={5} >
+          <Grid container spacing={5} >
           </Grid>
       </Container>
       {/* Footer */}
       <footer className={classes.footer}>
         <Container maxWidth="lg">
           <Typography variant="h6" align="center" gutterBottom>
-            Footer
+            Fanlinc
           </Typography>
           <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
             Linking Fans Together
