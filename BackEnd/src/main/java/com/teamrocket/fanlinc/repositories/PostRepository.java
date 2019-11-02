afd92ce0a1f44e72b38cc9afdae127c5aad20573
RahulRamani23 <39231376+RahulRamani23@users.noreply.github.com>
2019-11-02 15:41:02 -0400

    TEAM-42 - Set up method in PostRepository repo to obtain the required posts

--- BackEnd/src/main/java/com/teamrocket/fanlinc/repositories/PostRepository.java
+++ BackEnd/src/main/java/com/teamrocket/fanlinc/repositories/PostRepository.java
@@ -1,8 +1,11 @@
 package com.teamrocket.fanlinc.repositories;
 
 import com.teamrocket.fanlinc.models.Post;
+import java.util.List;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface PostRepository extends Neo4jRepository<Post, Long> {
+
+  List<Post> findByFandomNameAndLevelAndTypeOrderByPostedTimeDesc(String fandom, String level, String Type);
 
 }
