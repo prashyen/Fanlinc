f8d9837c71e531c1af3ebaa63422daca466efbdc
CrypticH3RO <aamir.haroon@mail.utoronto.ca>
2019-11-03 00:11:02 -0400

    Added error check if user is in fandom

--- BackEnd/src/main/java/com/teamrocket/fanlinc/exceptions/UserNotInFandomException.java
+++ BackEnd/src/main/java/com/teamrocket/fanlinc/exceptions/UserNotInFandomException.java
@@ -1,4 +1,11 @@
 package com.teamrocket.fanlinc.exceptions;
 
-public class UserNotInFandomException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserNotInFandomException extends RuntimeException {
+  public UserNotInFandomException(String message) {
+    super(message);
+  }
 }
--- BackEnd/src/main/java/com/teamrocket/fanlinc/services/PostService.java
+++ BackEnd/src/main/java/com/teamrocket/fanlinc/services/PostService.java
@@ -3,10 +3,13 @@ package com.teamrocket.fanlinc.services;
 import com.teamrocket.fanlinc.builders.PostBuilder;
 import com.teamrocket.fanlinc.exceptions.FandomNotFoundException;
 import com.teamrocket.fanlinc.exceptions.InvalidLevelException;
+import com.teamrocket.fanlinc.exceptions.UserNotInFandomException;
 import com.teamrocket.fanlinc.exceptions.UserNotFoundException;
 import com.teamrocket.fanlinc.models.Fandom;
+import com.teamrocket.fanlinc.models.Joined;
 import com.teamrocket.fanlinc.models.User;
 import com.teamrocket.fanlinc.repositories.FandomRepository;
+import com.teamrocket.fanlinc.repositories.JoinedRepository;
 import com.teamrocket.fanlinc.repositories.PostRepository;
 import com.teamrocket.fanlinc.repositories.UserRepository;
 import com.teamrocket.fanlinc.requests.AddPostRequest;
@@ -24,6 +27,7 @@ public class PostService {
   private PostRepository postRepository;
   private FandomRepository fandomRepository;
   private UserRepository userRepository;
+  private JoinedRepository joinedRepository;
   private final List<String> levels = Arrays.asList(new String[] {"1", "2", "3", "4", "noFilter"});
   private final List<String> types =
       Arrays.asList(new String[] {"General", "Cosplayer", "Vendor/Artist", "noFilter"});
@@ -31,16 +35,17 @@ public class PostService {
   public PostService(
       PostRepository postRepository,
       FandomRepository fandomRepository,
-      UserRepository userRepository) {
+      UserRepository userRepository,
+      JoinedRepository joinedRepository) {
     this.postRepository = postRepository;
     this.fandomRepository = fandomRepository;
     this.userRepository = userRepository;
+    this.joinedRepository = joinedRepository;
   }
 
   @Transactional(readOnly = false)
   public AddPostResponse addPost(AddPostRequest request) {
     Fandom requestedFandom = fandomRepository.findByFandomName(request.getFandomName());
-    User requestedUser = userRepository.findByUsername(request.getPostedBy());
     // ensure the requested fandom has already been created
     if (requestedFandom == null) {
       // Fandom has not been created yet
@@ -54,10 +59,19 @@ public class PostService {
     if (!types.contains(request.getType())) {
       throw new InvalidLevelException(request.getType() + " is not a valid type");
     }
+    User requestedUser = userRepository.findByUsername(request.getPostedBy());
     // ensure the requested username exists
     if (requestedUser == null) {
       throw new UserNotFoundException(
           "User with username " + request.getPostedBy() + " does not exist");
+    }
+    Joined requestedRelation =
+        joinedRepository.findJoinedByUsernameAndFandomName(
+            request.getPostedBy(), request.getFandomName());
+    // ensure the user is already a member of the fandom
+    if (requestedRelation == null) {
+      // User already a member of the fandom, output exception
+      throw new UserNotInFandomException("User not in " + request.getFandomName());
     }
     postRepository.save(
         new PostBuilder()
